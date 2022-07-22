9d9d2f6cee45deee6f71fa4c611a956678f10cec
Ben Law <bsblaw@gmail.com>
2022-07-22 19:17:51 +0000

    add hello function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ function createNewSheet(dataFolderName) {
   const {newSheetName, masterStockList} = getStockListFromDrive(dataFolderName);
   presentToSheet(masterStockList, newSheet(newSheetName));
 }
+
+function hello() { Logger.log("hello"); }
